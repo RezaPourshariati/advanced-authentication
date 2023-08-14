cd93eb674feaf59e83017cee9fe158dcad173e96
Reza Pourshariati <rezaporshariati@gmail.com>
2023-08-14 23:53:12 +0330

    redux setup.

---
+++ front-end/src/redux/features/auth/authSlice.js
@@ -0,0 +1,41 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import authService from "./authService";
+import {toast} from './authService';
+
+const initialState = {
+    isLoggedIn: false,
+    user: null,
+    users: [],
+    twoFactor: false,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ""
+};
+
+// Register User
+export const register = createAsyncThunk("auth/register", async (userData, thunkAPI) => {
+    try {
+        return await authService.register(userData);
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+        return thunkAPI.rejectWithValue(message);
+    }
+});
+
+const authSlice = createSlice({
+    name: "auth", initialState, reducers: {
+        RESET(state) {
+            state.twoFactor = false;
+            state.isError = false;
+            state.isSuccess = false;
+            state.isLoading = false;
+            state.message = "";
+        }
+    }
+});
+
+export const {RESET} = authSlice.actions;
+export const selectEsLoggedIn = (state) => state.auth.isLoggedIn;
+
+export default authSlice.reducer;
