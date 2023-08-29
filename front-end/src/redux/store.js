10f50a891bf9ec61e5b6fb7fca535f294276575d
Reza Pourshariati <rezaporshariati@gmail.com>
2023-08-29 23:39:31 +0330

    redux store modified.

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
-import authReducer from '../redux/features/auth/authSlice';
+import authReducer from './features/auth/authSlice';
+import emailReducer from "./features/email/emailSlice";
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        email: emailReducer
     }
 });
