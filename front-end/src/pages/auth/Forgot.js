31618bd693872c978afdb92a7afa08d9fc358130
Reza Pourshariati <rezaporshariati@gmail.com>
2023-08-31 00:34:46 +0330

    forgot password updated.

--- front-end/src/pages/auth/Forgot.js
+++ front-end/src/pages/auth/Forgot.js
@@ -3,21 +3,32 @@ import Card from "../../components/card/Card";
 import styles from './auth.module.scss';
 import {Link} from "react-router-dom";
 import {AiOutlineMail} from "react-icons/ai";
+import {toast} from "react-toastify";
+import {validateEmail} from "../../redux/features/auth/authService";
+import {useDispatch, useSelector} from "react-redux";
+import {forgotPassword, RESET} from "../../redux/features/auth/authSlice";
+import Loader from "../../components/loader/Loader";
 
 const Forgot = () => {
     const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
 
-    const handleInputChange = (e) => {
+    const { isLoading } = useSelector((state) => state.auth);
 
-    };
-
-    const loginUser = (e) => {
+    const forgot = async (e) => {
         e.preventDefault();
+        if (!email) return toast.error("Please enter an email");
+        if (!validateEmail(email)) return toast.error("Please enter a valid email");
+        const userData = {
+            email
+        };
+        await dispatch(forgotPassword(userData));
+        await dispatch(RESET(userData));
     };
 
     return (
         <>
+            {isLoading && <Loader />}
             <div className={`container ${styles.auth}`}>
                 <Card>
                     <div className={styles.form}>
@@ -26,10 +37,10 @@ const Forgot = () => {
                         </div>
                         <h2 style={{marginBottom: '4rem'}}>Forgot Password</h2>
 
-                        <form onSubmit={loginUser}>
+                        <form onSubmit={forgot}>
                             <label htmlFor="email"><p>Please Enter Email Address for Resetting Password!</p></label>
                             <input type="email" placeholder='Email' name='email' value={email}
-                                   onChange={handleInputChange} required/>
+                                   onChange={(e) => setEmail(e.target.value)} required/>
                             <button type='submit' className='--btn --btn-primary --btn-block'>Get Reset Email</button>
 
                             <div className={styles.links}>
