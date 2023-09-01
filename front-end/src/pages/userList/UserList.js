70691588d82c935f02a6c7845db30e716ecf3e53
Reza Pourshariati <rezaporshariati@gmail.com>
2023-09-01 22:17:40 +0330

    change role functionality done.

--- front-end/src/pages/userList/UserList.js
+++ front-end/src/pages/userList/UserList.js
@@ -6,19 +6,22 @@ import {FaTrashAlt} from "react-icons/fa";
 import ChangeRole from "../../components/changeRole/ChangeRole";
 import {useDispatch, useSelector} from "react-redux";
 import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
-import {useEffect} from "react";
-import {deleteUser, getUsers} from "../../redux/features/auth/authSlice";
+import {useEffect, useState} from "react";
+import {deleteUser, getUsers, selectUser} from "../../redux/features/auth/authSlice";
 import {shortenText} from "../profile/Profile";
 import {Spinner} from "../../components/loader/Loader";
 import {confirmAlert} from "react-confirm-alert";
 import 'react-confirm-alert/src/react-confirm-alert.css';
+import {FILTER_USERS, selectUsers} from "../../redux/features/auth/filterSlice";
+import ReactPaginate from "react-paginate";
 
 
 const UserList = () => {
     useRedirectLoggedOutUser("/login"); // Custom Hook
     const dispatch = useDispatch();
-
+    const [search, setSearch] = useState("");
     const {users, isLoading, isLoggedIn, isSuccess, message} = useSelector(state => state.auth);
+    const filteredUsers = useSelector(selectUsers);
 
     useEffect(() => {
         dispatch(getUsers());
@@ -40,18 +43,41 @@ const UserList = () => {
                 },
                 {
                     label: 'Cancel',
-                    onClick: () => alert('Click No')
+                    onClick: () => alert('Click Ok')
                 }
             ]
         });
     };
+
+    useEffect(() => {
+        dispatch(FILTER_USERS({users, search}));
+    }, [dispatch, users, search]);
+
+
+    // -------------- Begin Pagination
+    const itemsPerPage = 5;
+    const [itemOffset, setItemOffset] = useState(0);
+
+    const endOffset = itemOffset + itemsPerPage;
+    // console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+
+    const currentItems = filteredUsers.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(filteredUsers.length / itemsPerPage);
+
+    // Invoke when user click to request another page.
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % filteredUsers.length;
+        // console.log(`User requested page number ${event.selected}, which is offset ${newOffset}`);
+        setItemOffset(newOffset);
+    };
+    // -------------- End Pagination
 
     return (
         <>
             <section>
                 <div className="container">
                     <PageMenu/>
-                    {/*<UserStats/>*/}
+                    <UserStats/>
 
                     <div className="user-list">
                         {isLoading && <Spinner/>}
@@ -60,7 +86,7 @@ const UserList = () => {
                                 <span>
                                     <h3>All Users</h3>
                                 </span>
-                                <span><Search/></span>
+                                <span><Search value={search} onChange={(e) => setSearch(e.target.value)}/></span>
                             </div>
 
                             {/* Table */}
@@ -78,7 +104,7 @@ const UserList = () => {
                                     </thead>
 
                                     <tbody>
-                                    {users.map((user, index) => {
+                                    {currentItems.map((user, index) => {
                                         const {_id, name, email, role} = user;
                                         return (
                                             <tr key={_id}>
@@ -89,7 +115,7 @@ const UserList = () => {
                                                 <td><ChangeRole id={_id} email={email}/></td>
                                                 <td>
                                                     <span className='icon'><FaTrashAlt size={20} color={"#c81d25"}
-                                                                      onClick={() => confirmDelete(_id)}/></span>
+                                                                                       onClick={() => confirmDelete(_id)}/></span>
                                                 </td>
                                             </tr>
                                         );
@@ -97,7 +123,22 @@ const UserList = () => {
                                     </tbody>
                                 </table>
                             )}
+                            <hr color='greenyellow'/>
                         </div>
+                        <ReactPaginate
+                            breakLabel="..."
+                            nextLabel="Next >"
+                            onPageChange={handlePageClick}
+                            pageRangeDisplayed={5}
+                            pageCount={pageCount}
+                            previousLabel="Prev"
+                            renderOnZeroPageCount={null}
+                            containerClassName="pagination"
+                            pageLinkClassName="page-num"
+                            previousLinkClassName="page-num"
+                            nextLinkClassName="page-num"
+                            activeLinkClassName="activePage"
+                        />
                     </div>
                 </div>
             </section>
