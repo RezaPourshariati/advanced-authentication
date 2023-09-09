bc7d30ba4684200d88b98acb44abcf350ccfe665
Reza Pourshariati <rezaporshariati@gmail.com>
2023-09-09 13:53:11 +0330

    server modified.

--- back-end/server.js
+++ back-end/server.js
@@ -15,9 +15,9 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "https://authreza-app.vercel.app"],
+    origin: ["http://localhost:3000"],
     credentials: true
-})); // cors help us prevent any conflict when we make a request from front-end to the back-end.
+}));
 
 
 // Routes
