3cbd0870c51aeaf4730d5f549be666328563e491
رضا پورشریعتی <rpourshariati@noornent.net>
2025-12-04 15:25:59 +0330

    fix(client): change some styles path in assets directory

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,6 +1,9 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { store } from './redux/store'
-import './index.css'
+import App from './App'
+import '@/assets/styles/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
